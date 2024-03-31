d0a66d4ddf09008f12fd0c3e5c52c808f20d491e
Kame-Bashuna <basshunakameroba@gmail.com>
2024-03-31 20:09:56 +0300

    add changes

---
+++ stacks.py
@@ -0,0 +1,17 @@
+#create stack
+names=[]
+names.append('aggy')
+names.append('alex')
+names.append('hope')
+print(names)
+
+#access item in stack
+print(names[1])
+
+#add item in stack
+names.append('faith')
+print(names)
+
+#remove item from stack
+names.pop()
+print(names)
